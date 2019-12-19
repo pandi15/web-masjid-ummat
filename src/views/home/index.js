89c298b0462556d352768c1c96827efb1c100a51
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2019-12-19 22:45:22 +0700

    Change header

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,37 +1,14 @@
 import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom'
 
+import HeaderComponent from 'components/HeaderComponent'
 import mosqueImage from '../../assets/images/mosque.svg'
 
 class Home extends PureComponent {
   render () {
     return (
       <>
-        <nav className="navbar main-navbar">
-          <div className="container">
-            <Link className="navbar-brand" to="#">MasjidUmmat</Link>
-            <div className="navbar-menu ml-auto mr-5">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link href="#">Menu 1</Link>
-                </li>
-                <li className="nav-item">
-                  <Link href="#">Menu 2</Link>
-                </li>
-                <li className="nav-item">
-                  <Link href="#">Menu 3</Link>
-                </li>
-                <li className="nav-item">
-                  <Link href="#">Menu 4</Link>
-                </li>
-              </ul>
-            </div>
-            <div className="navbar-menu">
-              <Link href="#" className="btn btn-main">Masuk</Link>
-            </div>
-          </div>
-        </nav>
-
+        <HeaderComponent />
         <div className="header">
           <div className="header-content">
             <div className="container">
